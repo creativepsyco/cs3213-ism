333816172946631ac1a6a2d8302f039c9b9abd2c
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-11-30 15:14:56 +0800

    init

---
+++ widget_framework/backbone-widget/backbone-widget-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneWidgetApp::Application.config.secret_token = '4b8c0fdcf7548285269abbde6378a313714d5491bcec025a23642700854ff969fc6548e42bcac75be876882ada7e482819e2fdff1b9af960bbea5dae1efd9e4e'
